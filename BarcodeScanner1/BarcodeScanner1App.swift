2481b046c97a1d02e30ddaabb73bc21217738995
Maimai10808 <331686549@qq.com>
2025-03-08 17:09:05 +0800

    Initial Commit

---
+++ BarcodeScanner1/BarcodeScanner1App.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScanner1App.swift
+//  BarcodeScanner1
+//
+//  Created by mac on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScanner1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
